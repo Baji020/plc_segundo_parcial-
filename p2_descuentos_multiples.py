0ee158b95a6268d6431fa829431248dcf1c389c2
Baji020 <145026343+Baji020@users.noreply.github.com>
2023-10-14 19:39:14 -0600

    Update p2_descuentos_multiples.py

--- p2_descuentos_multiples.py
+++ p2_descuentos_multiples.py
@@ -2,13 +2,18 @@
 
 x = float(input("Dame el precio del producto: "))
 y = float(input("Cuantos quieres: "))
-z = input("Que categoria es (A, B o C)")
-if z == "A" or z == "a":
-  e = (x * y) * 0.10
-  print("El total es de", e)
-elif z == "b" or z == "B":
-  e = (x * y) * 0.05
-  print("El,total es de", e)
-elif z == "c" or z == "C":
-  e = (x * y) * 0.02
-  print("El total es de", e)
+z = input("Que categoria es (A, B o C):")
+
+if z.lower() == "a":
+    s = 0.10
+elif z.lower() == "b":
+    s = 0.05
+elif z.lower() == "c":
+    s = 0.02
+
+f = x * y * s
+print("El total es de", f)
+
+if y > 10:
+    j = f * 0.05
+    print("Tu total es de", j)
